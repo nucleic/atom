bbe1618b7a18e8ab76f7d42a35d0702b3c12f8c8
Chris Colbert <sccolbert@gmail.com>
2013-03-06 16:10:58 -0500

    bring Tuple up-to-date

--- atom/tuple.py
+++ atom/tuple.py
@@ -5,19 +5,36 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from .catom import Member, DEFAULT_FACTORY, DEFAULT_VALUE, VALIDATE_TUPLE
+from .catom import Member, DefaultValue, Validate
+from .instance import Instance
 
 
 class Tuple(Member):
-    """ A value of type `tuple`.
+    """ A member which allows tuple values.
+
+    If item validation is used, then assignment will create a copy of
+    the original tuple before validating the items, since validation
+    may change the item values.
 
     """
     __slots__ = ()
 
-    def __init__(self, default=(), factory=None):
-        if factory is not None:
-            self.set_default_kind(DEFAULT_FACTORY, factory)
-        else:
-            self.set_default_kind(DEFAULT_VALUE, default)
-        self.set_validate_kind(VALIDATE_TUPLE, None)
+    def __init__(self, item=None, default=()):
+        """ Initialize a Tuple.
 
+        Parameters
+        ----------
+        item : Member, type, or tuple of types, optional
+            A member to use for validating the types of items allowed in
+            the tuple. This can also be a type object or a tuple of types,
+            in which case it will be wrapped with an Instance member. If
+            this is not given, no item validation is performed.
+
+        default : tuple, optional
+            The default tuple of values.
+
+        """
+        if item is not None and not isinstance(item, Member):
+            item = Instance(item)
+        self.set_default_value_mode(DefaultValue.Static, default)
+        self.set_validate_mode(Validate.Tuple, item)
