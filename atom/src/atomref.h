ebef5128a8acaaeb260be9c015866968b68d61d5
Chris Colbert <sccolbert@gmail.com>
2013-05-13 20:25:21 -0400

    Add an extension class that exposes the guard pointer
    This adds the AtomRef python extension class which allows a developer to
    keep a weak reference to an Atom object, regardless of whether or not
    that object is actually weak referenceable. Unlike a normal weakref,
    callbacks on object destruction are not supported.

---
+++ atom/src/atomref.h
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2013, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+extern PyTypeObject AtomRef_Type;
+
+int
+import_atomref();
