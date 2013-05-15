a16fe6f2c5e99ff5048e0e8c559128cd3c3e26a1
Chris Colbert <sccolbert@gmail.com>
2013-05-15 12:57:44 -0400

    add the remainder of the list validation methods.

--- atom/src/atomlist.cpp
+++ atom/src/atomlist.cpp
@@ -64,7 +64,7 @@ AtomList_dealloc( AtomList* self )
 
 
 static PyObject*
-AtomList_append( AtomList* self, PyObject* value )
+validate_single( AtomList* self, PyObject* value )
 {
     PyObjectPtr item( newref( value ) );
     if( self->validator && !self->pointer->is_null() )
@@ -75,6 +75,45 @@ AtomList_append( AtomList* self, PyObject* value )
         if( !item )
             return 0;
     }
+    return item.release();
+}
+
+
+static PyObject*
+validate_sequence( AtomList* self, PyObject* value )
+{
+    PyObjectPtr item( newref( value ) );
+    if( self->validator && !self->pointer->is_null() )
+    {
+        // no validation needed for self[::-1] = self
+        if( pyobject_cast( self ) != value )
+        {
+            PyListPtr templist( PySequence_List( value ) );
+            if( !templist )
+                return 0;
+            Py_ssize_t size = templist.size();
+            for( Py_ssize_t i = 0; i < size; ++i )
+            {
+                PyObject* val = self->validator->full_validate(
+                    self->pointer->data(), Py_None, templist.borrow_item( i )
+                );
+                if( !val )
+                    return 0;
+                templist.set_item( i, val );
+            }
+            item = templist;
+        }
+    }
+    return item.release();
+}
+
+
+static PyObject*
+AtomList_append( AtomList* self, PyObject* value )
+{
+    PyObjectPtr item( validate_single( self, value ) );
+    if( !item )
+        return 0;
     return ListMethods::append( pyobject_cast( self ), item.get() );
 }
 
@@ -82,25 +121,18 @@ AtomList_append( AtomList* self, PyObject* value )
 static PyObject*
 AtomList_insert( AtomList* self, PyObject* args )
 {
-    PyObjectPtr item( newref( args ) );
-    if( self->validator && !self->pointer->is_null() )
-    {
-        Py_ssize_t index;
-        PyObject* value;
-        if( !PyArg_ParseTuple( args, "nO:insert", &index, &value ) )
-            return 0;
-        value = self->validator->full_validate(
-            self->pointer->data(), Py_None, value
-        );
-        if( !value )
-            return 0;
-        // value is now an owned reference
-        item = PyTuple_New( 2 );
-        if( !item )
-            return 0;
-        PyTuple_SET_ITEM( item.get(), 0, PyInt_FromSsize_t( index ) );
-        PyTuple_SET_ITEM( item.get(), 1, value );
-    }
+    Py_ssize_t index;
+    PyObject* value;
+    if( !PyArg_ParseTuple( args, "nO:insert", &index, &value ) )
+        return 0;
+    value = validate_single( self, value );
+    if( !value )
+        return 0;
+    PyObjectPtr item( PyTuple_New( 2 ) );
+    if( !item )
+        return 0;
+    PyTuple_SET_ITEM( item.get(), 0, PyInt_FromSsize_t( index ) );
+    PyTuple_SET_ITEM( item.get(), 1, value );
     return ListMethods::insert( pyobject_cast( self ), item.get() );
 }
 
@@ -108,25 +140,90 @@ AtomList_insert( AtomList* self, PyObject* args )
 static PyObject*
 AtomList_extend( AtomList* self, PyObject* value )
 {
-    PyObjectPtr item( newref( value ) );
-    if( self->validator && !self->pointer->is_null() )
-    {
-        PyListPtr templist( PyList_New( 0 ) );
-        if( !templist.extend( value ) )
-            return 0;
-        Py_ssize_t size = templist.size();
-        for( Py_ssize_t i = 0; i < size; ++i )
-        {
-            PyObject* value = self->validator->full_validate(
-                self->pointer->data(), Py_None, templist.borrow_item( i )
-            );
-            if( !value )
-                return 0;
-            templist.set_item( i, value );
-        }
-        item = templist;
-    }
+    PyObjectPtr item( validate_sequence( self, value ) );
+    if( !item )
+        return 0;
     return ListMethods::extend( pyobject_cast( self ), item.get() );
+}
+
+
+static PyObject*
+AtomList_reduce_ex( AtomList* self, PyObject* proto )
+{
+    // An atomlist is pickled as a normal list. When the Atom class is
+    // reconstituted, assigning the list to the attribute will create
+    // a new atomlist with the proper owner. There is no need to try
+    // to persist the validator and pointer information.
+    PyObjectPtr data( PySequence_List( pyobject_cast( self ) ) );
+    if( !data )
+        return 0;
+    PyTuplePtr res( PyTuple_New( 2 ) );
+    if( !res )
+        return 0;
+    PyTuplePtr args( PyTuple_New( 1 ) );
+    if( !args )
+        return 0;
+    args.set_item( 0, data );
+    res.set_item( 0, newref( pyobject_cast( &PyList_Type ) ) );
+    res.set_item( 1, args );
+    return res.release();
+}
+
+
+static int
+AtomList_ass_item( AtomList* self, Py_ssize_t index, PyObject* value )
+{
+    if( !value )
+        return PyList_Type.tp_as_sequence->sq_ass_item(
+            pyobject_cast( self ), index, value );
+    PyObjectPtr item( validate_single( self, value ) );
+    if( !item )
+        return -1;
+    return PyList_Type.tp_as_sequence->sq_ass_item(
+        pyobject_cast( self ), index, item.get() );
+}
+
+
+static int
+AtomList_ass_slice( AtomList* self, Py_ssize_t low, Py_ssize_t high, PyObject* value )
+{
+    if( !value )
+        return PyList_Type.tp_as_sequence->sq_ass_slice(
+            pyobject_cast( self ), low, high, value );
+    PyObjectPtr item( validate_sequence( self, value ) );
+    if( !item )
+        return -1;
+    return PyList_Type.tp_as_sequence->sq_ass_slice(
+        pyobject_cast( self ), low, high, item.get() );
+}
+
+
+static PyObject*
+AtomList_inplace_concat( AtomList* self, PyObject* value )
+{
+    PyObjectPtr item( validate_sequence( self, value ) );
+    if( !item )
+        return 0;
+    return PyList_Type.tp_as_sequence->sq_inplace_concat(
+        pyobject_cast( self ), item.get() );
+}
+
+
+static int
+AtomList_ass_subscript( AtomList* self, PyObject* key, PyObject* value )
+{
+    if( !value )
+        return PyList_Type.tp_as_mapping->mp_ass_subscript(
+            pyobject_cast( self ), key, value );
+    PyObjectPtr item;
+    if( PyIndex_Check( key ) )
+        item = validate_single( self, value );
+    else if( PySlice_Check( key ) )
+        item = validate_sequence( self, value );
+    else
+        item = newref( value );
+    return PyList_Type.tp_as_mapping->mp_ass_subscript(
+        pyobject_cast( self ), key, value );
 }
 
 
@@ -143,7 +240,31 @@ AtomList_methods[] = {
     { "append", ( PyCFunction )AtomList_append, METH_O, append_doc },
     { "insert", ( PyCFunction )AtomList_insert, METH_VARARGS, insert_doc },
     { "extend", ( PyCFunction )AtomList_extend, METH_O, extend_doc },
-    { 0 }           /* sentinel */
+    { "__reduce_ex__", ( PyCFunction )AtomList_reduce_ex, METH_O, "" },
+    { 0 }  /* sentinel */
+};
+
+
+static PySequenceMethods
+AtomList_as_sequence = {
+    (lenfunc)0,                                 /* sq_length */
+    (binaryfunc)0,                              /* sq_concat */
+    (ssizeargfunc)0,                            /* sq_repeat */
+    (ssizeargfunc)0,                            /* sq_item */
+    (ssizessizeargfunc)0,                       /* sq_slice */
+    (ssizeobjargproc)AtomList_ass_item,         /* sq_ass_item */
+    (ssizessizeobjargproc)AtomList_ass_slice,   /* sq_ass_slice */
+    (objobjproc)0,                              /* sq_contains */
+    (binaryfunc)AtomList_inplace_concat,        /* sq_inplace_concat */
+    (ssizeargfunc)0,                            /* sq_inplace_repeat */
+};
+
+
+static PyMappingMethods
+AtomList_as_mapping = {
+    (lenfunc)0,                             /* mp_length */
+    (binaryfunc)0,                          /* mp_subscript */
+    (objobjargproc)AtomList_ass_subscript   /* mp_ass_subscript */
 };
 
 
@@ -160,8 +281,8 @@ PyTypeObject AtomList_Type = {
     (cmpfunc)0,                             /* tp_compare */
     (reprfunc)0,                            /* tp_repr */
     (PyNumberMethods*)0,                    /* tp_as_number */
-    (PySequenceMethods*)0,                  /* tp_as_sequence */
-    (PyMappingMethods*)0,                   /* tp_as_mapping */
+    (PySequenceMethods*)&AtomList_as_sequence, /* tp_as_sequence */
+    (PyMappingMethods*)&AtomList_as_mapping,   /* tp_as_mapping */
     (hashfunc)0,                            /* tp_hash */
     (ternaryfunc)0,                         /* tp_call */
     (reprfunc)0,                            /* tp_str */
@@ -208,7 +329,7 @@ AtomList_New( Py_ssize_t size, CAtom* atom, Member* validator )
     // slave list is allocated and it's pointer array is stolen. This
     // ensures the pointer array is malloced, resized, and freed by
     // the same CRT.
-    PyListObject* slave = reinterpret_cast<PyListObject*>( PyList_New( size ) );
+    PyObjectPtr slave( PyList_New( size ) );
     if( !slave )
         return 0;
 
@@ -217,14 +338,14 @@ AtomList_New( Py_ssize_t size, CAtom* atom, Member* validator )
         return 0;
 
     PyListObject* op = reinterpret_cast<PyListObject*>( ptr.get() );
-    op->ob_item = slave->ob_item;
+    PyListObject* slave_op = reinterpret_cast<PyListObject*>( slave.get() );
+    op->ob_item = slave_op->ob_item;
     op->allocated = size;
     Py_SIZE( op ) = size;
 
-    slave->ob_item = 0;
-    slave->allocated = 0;
-    Py_SIZE( slave ) = 0;
-    Py_DECREF( slave );
+    slave_op->ob_item = 0;
+    slave_op->allocated = 0;
+    Py_SIZE( slave_op ) = 0;
 
     Py_XINCREF( pyobject_cast( validator ) );
     atomlist_cast( ptr.get() )->validator = validator;
