0c2e4a12d4235a56d9e5620c681576e8550f4a34
blink1073 <steven.silvester@ieee.org>
2013-10-26 15:02:38 -0500

    Initial commit of alias type stub

---
+++ atom/alias.py
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from .catom import Member, DefaultValue, Validate
+
+
+class Alias(object):
+    """A descriptor that forbids negative values"""
+    
+    __slots__ = ()
+    
+    def __init__(self, default):
+        self.default = default
+        
+    def __get__(self, instance, owner):
+        # we get here when someone calls x.d, and d is a NonNegative instance
+        # instance = x
+        # owner = type(x)
+        return self.data.get(instance, self.default)
+    
+    def __set__(self, instance, value):
+        # we get here when someone calls x.d = val, and d is a NonNegative instance
+        # instance = x
+        # value = val
+        if value < 0:
+            raise ValueError("Negative value not allowed: %s" % value)
+        self.data[instance] = value
