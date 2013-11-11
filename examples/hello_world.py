9ac7d2c9493acdd384636b1f7a2265dbc9b708de
blink1073 <steven.silvester@ieee.org>
2013-11-11 08:00:46 -0600

    Add hello world example based on enaml tutorial example

---
+++ examples/hello_world.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from __future__ import print_function
+from atom.api import Atom, Unicode
+
+
+class Hello(Atom):
+
+    message = Unicode('Hello')
+
+
+if __name__ == "__main__":
+
+    hello = Hello()
+    print(hello.message)
+    hello.message = 'Goodbye'
+    print(hello.message)
