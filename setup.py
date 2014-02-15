58d2c1b0ac0f9f71e18c1d6a79b3b523f260c141
S. Chris Colbert <sccolbert@gmail.com>
2014-02-15 16:00:16 -0500

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -6,32 +6,34 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
 from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
 
 
 ext_modules = [
     Extension(
         'atom.catom',
-        ['atom/src/atomlist.cpp',
-         #'atom/src/atomdict.cpp',
-         'atom/src/atomref.cpp',
-         'atom/src/catom.cpp',
-         'atom/src/catommodule.cpp',
-         'atom/src/defaultvaluebehavior.cpp',
-         'atom/src/delattrbehavior.cpp',
-         'atom/src/enumtypes.cpp',
-         'atom/src/eventbinder.cpp',
-         'atom/src/getattrbehavior.cpp',
-         'atom/src/member.cpp',
-         'atom/src/memberchange.cpp',
-         'atom/src/methodwrapper.cpp',
-         'atom/src/observerpool.cpp',
-         'atom/src/postgetattrbehavior.cpp',
-         'atom/src/postsetattrbehavior.cpp',
-         'atom/src/postvalidatebehavior.cpp',
-         'atom/src/setattrbehavior.cpp',
-         'atom/src/signalconnector.cpp',
-         'atom/src/validatebehavior.cpp',
-         ],
+        [
+            'atom/src/atomlist.cpp',
+            #'atom/src/atomdict.cpp',
+            'atom/src/atomref.cpp',
+            'atom/src/catom.cpp',
+            'atom/src/catommodule.cpp',
+            'atom/src/defaultvaluebehavior.cpp',
+            'atom/src/delattrbehavior.cpp',
+            'atom/src/enumtypes.cpp',
+            'atom/src/eventbinder.cpp',
+            'atom/src/getattrbehavior.cpp',
+            'atom/src/member.cpp',
+            'atom/src/memberchange.cpp',
+            'atom/src/methodwrapper.cpp',
+            'atom/src/observerpool.cpp',
+            'atom/src/postgetattrbehavior.cpp',
+            'atom/src/postsetattrbehavior.cpp',
+            'atom/src/postvalidatebehavior.cpp',
+            'atom/src/setattrbehavior.cpp',
+            'atom/src/signalconnector.cpp',
+            'atom/src/validatebehavior.cpp',
+        ],
         language='c++',
     ),
     Extension(
@@ -40,6 +42,22 @@ ext_modules = [
         language='c++',
     ),
 ]
+
+
+class BuildExt(build_ext):
+    """ A custom build extension for adding compiler-specific options.
+
+    """
+    c_opts = {
+        'msvc': ['/EHsc']
+    }
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
 
 
 setup(
@@ -53,4 +71,5 @@ setup(
     install_requires=['distribute'],
     packages=find_packages(),
     ext_modules=ext_modules,
+    cmdclass={'build_ext': BuildExt},
 )
