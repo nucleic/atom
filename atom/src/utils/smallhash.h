0d92b3c6a3e70a6c6fe4b59402fe4f2173ca93cb
S. Chris Colbert <sccolbert@gmail.com>
2014-08-10 20:54:17 -0400

    wip

--- atom/src/utils/smallhash.h
+++ atom/src/utils/smallhash.h
@@ -10,38 +10,58 @@
 #include <utils/math.h>
 #include <utils/stdint.h>
 
-#include <utility>
 #include <vector>
 
 
-template <typename Key, typename T, typename Hash, typename Pred>
+template <typename Key, typename Value, typename Hash, typename Equal>
 class SmallHash
 {
 
 public:
 
 	typedef Key key_type;
-	typedef T mapped_type;
-	typedef std::pair<const Key, T> value_type;
+	typedef Value value_type;
 
-	SmallHash( uint32_t size = 0 ) : m_data( alloc_size( size ) ) { }
+	SmallHash( uint32_t size = 0 ) : m_data( allocSize( size ) ) { }
+
+	Value* find( const Key& key )
+	{
+
+	}
+
+	void insert( const Key& key, const Value& value )
+	{
+
+	}
+
+	void remove( const Key& key )
+	{
+
+	}
 
 private:
 
 	struct Entry
 	{
 		enum State { Empty, Full, Deleted };
-		value_type value;
+		Key key;
+		Value value;
 		size_t hash;
 		State state;
 	};
 
-	static uint32_t alloc_size( uint32_t n )
+	static uint32_t allocSize( uint32_t n )
 	{
 	    static const uint32_t min_slots = 3;
 	    static const uint32_t min_alloc = 4;
 	    n = std::max( n, min_slots );
 	    return next_power_of_2( n * min_alloc / min_slots );
+	}
+
+	Entry* locate( const Key& key )
+	{
+		size_t hash = Hash()( key );
+
 	}
 
 	std::vector<Entry> m_entries;
