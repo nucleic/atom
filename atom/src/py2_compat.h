076f2150ae952ef6e1db324291005be493fbc43f
S. Chris Colbert <sccolbert@gmail.com>
2014-08-29 18:56:04 -0400

    prune unused compat code

--- atom/src/py2_compat.h
+++ atom/src/py2_compat.h
@@ -11,56 +11,10 @@
 #include <Python.h>
 
 
-#define Py23StrObject PyStringObject
 #define Py23Str_Check PyString_Check
-#define Py23Str_FromString PyString_FromString
 #define Py23Bytes_Check PyString_Check
 #define Py23Bytes_AS_STRING PyString_AS_STRING
 #define Py23Int_Check( ob ) ( PyInt_Check( ob ) || PyLong_Check( ob ) )
 #define Py23Int_FromLong PyInt_FromLong
 #define Py23Int_AsLong PyInt_AsLong
 #define Py23Number_Int PyNumber_Int
-
-
-inline size_t Py23Str_Hash( Py23StrObject* op )
-{
-	long hash = op->ob_shash;
-	if( hash == -1 )
-	{
-		hash = PyObject_Hash( reinterpret_cast<PyObject*>( op ) );
-	}
-	return static_cast<size_t>( hash );
-}
-
-
-inline bool Py23Str_Equal( Py23StrObject* a, Py23StrObject* b )
-{
-	if( a == b )
-	{
-		return true;
-	}
-	if( Py_SIZE( a ) == Py_SIZE( b ) )
-	{
-		return memcmp( a->ob_sval, b->ob_sval, Py_SIZE( a ) ) == 0;
-	}
-	return false;
-}
-
-
-inline PyObject* PyName_As23Str( PyObject* name )
-{
-	if( PyString_Check( name ) )
-	{
-		return cppy::incref( name );
-	}
-	if( PyUnicode_Check( name ) )
-	{
-		// CPython's name conversion policy
-		return PyUnicode_AsEncodedString( name, 0, 0 );
-	}
-	PyErr_Format(
-		PyExc_TypeError,
-		"attribute name must be string, not '%.200s'",
-		Py_TYPE( name )->tp_name );
-	return 0;
-}
--- atom/src/py3_compat.h
+++ atom/src/py3_compat.h
@@ -11,30 +11,10 @@
 #include <Python.h>
 
 
-#define Py23StrObject PyUnicodeObject
 #define Py23Str_Check PyUnicode_Check
-#define Py23Str_FromString PyUnicode_FromString
 #define Py23Bytes_Check PyBytes_Check
 #define Py23Bytes_AS_STRING PyBytes_AS_STRING
 #define Py23Int_Check PyLong_Check
 #define Py23Int_FromLong PyLong_FromLong
 #define Py23Int_AsLong PyLong_AsLong
 #define Py23Number_Int PyNumber_Long
-
-
-inline size_t Py23Str_Hash( Py23StrObject* op )
-{
-	// XXX implement me
-}
-
-
-inline bool Py23Str_Equal( Py23StrObject* a, Py23StrObject* b )
-{
-	// XXX implement me
-}
-
-
-inline PyObject* PyName_As23Str( PyObject* name )
-{
-	// XXX implement me
-}
