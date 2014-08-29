09f5bf1078881c1b17230b10bc12ee08a4edb0b8
S. Chris Colbert <sccolbert@gmail.com>
2014-08-29 14:09:52 -0400

    spaces -> tabs

--- atom/src/member.h
+++ atom/src/member.h
@@ -7,7 +7,7 @@
 |----------------------------------------------------------------------------*/
 #pragma once
 
-#include <utils/stdint.h>
+#include "stdint.h"
 
 #include <Python.h>
 
@@ -21,109 +21,87 @@ extern PyObject* ValidationError;
 // POD struct - all member fields are considered private
 struct Member
 {
-    PyObject_HEAD
-    PyObject* m_metadata;
-    PyObject* m_default_context;
-    PyObject* m_validate_context;
-    PyObject* m_post_validate_context;
-    PyObject* m_post_setattr_context;
-    uint32_t m_flags;
-    uint8_t m_default_mode;
-    uint8_t m_validate_mode;
-    uint8_t m_post_validate_mode;
-    uint8_t m_post_setattr_mode;
+	PyObject_HEAD
+	PyObject* m_metadata;
+	PyObject* m_default_context;
+	PyObject* m_validate_context;
+	PyObject* m_post_validate_context;
+	PyObject* m_post_setattr_context;
+	uint8_t m_default_mode;
+	uint8_t m_validate_mode;
+	uint8_t m_post_validate_mode;
+	uint8_t m_post_setattr_mode;
 
-    enum Flag
-    {
-    };
+	enum DefaultMode
+	{
+		NoDefault,
+		DefaultValue,
+		DefaultList,
+		DefaultDict,
+		DefaultFactory,
+		DefaultCallObject,
+		DefaultAtomMethod,
+		DefaultMemberMethod,
+		DefaultLast // sentinel
+	};
 
-    enum DefaultMode
-    {
-        NoDefault,
-        DefaultValue,
-        DefaultList,
-        DefaultDict,
-        DefaultFactory,
-        DefaultCallObject,
-        DefaultAtomMethod,
-        DefaultMemberMethod,
-        DefaultLast // sentinel
-    };
+	enum ValidateMode
+	{
+		NoValidate,
+		ValidateBool,
+		ValidateInt,
+		ValidateFloat,
+		ValidateBytes,
+		ValidateStr,
+		ValidateUnicode,
+		ValidateTuple,
+		ValidateList,
+		ValidateDict,
+		ValidateTyped,
+		ValidateInstance,
+		ValidateSubclass,
+		ValidateEnum,
+		ValidateCallable,
+		ValidateRange,
+		ValidateCoerced,
+		ValidateCallObject,
+		ValidateAtomMethod,
+		ValidateMemberMethod,
+		ValidateLast // sentinel
+	};
 
-    enum ValidateMode
-    {
-        NoValidate,
-        ValidateBool,
-        ValidateInt,
-        ValidateFloat,
-        ValidateBytes,
-        ValidateStr,
-        ValidateUnicode,
-        ValidateTuple,
-        ValidateList,
-        ValidateDict,
-        ValidateTyped,
-        ValidateInstance,
-        ValidateSubclass,
-        ValidateEnum,
-        ValidateCallable,
-        ValidateRange,
-        ValidateCoerced,
-        ValidateCallObject,
-        ValidateAtomMethod,
-        ValidateMemberMethod,
-        ValidateLast // sentinel
-    };
+	enum PostValidateMode
+	{
+		NoPostValidate,
+		PostValidateCallObject,
+		PostValidateAtomMethod,
+		PostValidateMemberMethod,
+		PostValidateLast // sentinel
+	};
 
-    enum PostValidateMode
-    {
-        NoPostValidate,
-        PostValidateCallObject,
-        PostValidateAtomMethod,
-        PostValidateMemberMethod,
-        PostValidateLast // sentinel
-    };
+	enum PostSetAttrMode
+	{
+		NoPostSetAttr,
+		PostSetAttrCallObject,
+		PostSetAttrAtomMethod,
+		PostSetAttrMemberMethod,
+		PostSetAttrLast // sentinel
+	};
 
-    enum PostSetAttrMode
-    {
-        NoPostSetAttr,
-        PostSetAttrCallObject,
-        PostSetAttrAtomMethod,
-        PostSetAttrMemberMethod,
-        PostSetAttrLast // sentinel
-    };
+	static PyTypeObject TypeObject;
 
-    static PyTypeObject TypeObject;
+	static bool Ready();
 
-    static bool Ready();
+	static bool TypeCheck( PyObject* ob )
+	{
+		return PyObject_TypeCheck( ob, &TypeObject ) != 0;
+	}
 
-    static bool TypeCheck( PyObject* ob )
-    {
-        return PyObject_TypeCheck( ob, &TypeObject ) != 0;
-    }
+	PyObject* defaultValue( PyObject* atom, PyObject* name );
 
-    bool testFlag( Flag flag )
-    {
-        return ( m_flags & static_cast<uint32_t>( flag ) ) != 0;
-    }
+	PyObject* validateValue( PyObject* atom, PyObject* name, PyObject* value );
 
-    bool setFlag( Flag flag, bool on = true )
-    {
-        if( on )
-        {
-            m_flags |= static_cast<uint32_t>( flag );
-        }
-        else
-        {
-            m_flags &= ~( static_cast<uint32_t>( flag ) );
-        }
-    }
-
-    PyObject* defaultValue( PyObject* atom, PyObject* name );
-
-    PyObject* validateValue( PyObject* atom, PyObject* name, PyObject* value );
-
-    int postSetAttrValue( PyObject* atom, PyObject* name, PyObject* value );
+	int postSetAttrValue( PyObject* atom, PyObject* name, PyObject* value );
 };
 
 } // namespace atom
