d79c14c62ed57bb4bad8246be1191a6e377d4fc9
S. Chris Colbert <sccolbert@gmail.com>
2014-09-02 22:52:07 -0400

    merge safe utils into callback set module
    only this module uses them

--- atom/src/callback_set.cpp
+++ atom/src/callback_set.cpp
@@ -6,11 +6,100 @@
 | The full license is in the file COPYING.txt, distributed with this software.
 |----------------------------------------------------------------------------*/
 #include "callback_set.h"
-#include "safe_utils.h"
 
 
 namespace atom
 {
+
+namespace
+{
+
+inline void printAndClearError()
+{
+	if( PyErr_Occurred() )
+	{
+		PyErr_Print();
+	}
+}
+
+
+inline bool safeCmpEqual( PyObject* a, PyObject* b )
+{
+	int ok = PyObject_RichCompareBool( a, b, Py_EQ );
+	if( ok == 1 )
+	{
+		return true;
+	}
+	if( ok == 0 )
+	{
+		return false;
+	}
+	printAndClearError();
+	return false;
+}
+
+
+inline bool safeIsTruthy( PyObject* pyo )
+{
+	int ok = PyObject_IsTrue( pyo );
+	if( ok == 1 )
+	{
+		return true;
+	}
+	if( ok == 0 )
+	{
+		return false;
+	}
+	printAndClearError();
+	return false;
+}
+
+
+inline bool safeSetContains( PyObject* set, PyObject* value )
+{
+	int ok = PySet_Contains( set, value );
+	if( ok == 1 )
+	{
+		return true;
+	}
+	if( ok == 0 )
+	{
+		return false;
+	}
+	printAndClearError();
+	return false;
+}
+
+
+inline void safeSetAdd( PyObject* set, PyObject* value )
+{
+	if( PySet_Add( set, value ) < 0 )
+	{
+		printAndClearError();
+	}
+}
+
+
+inline void safeSetDiscard( PyObject* set, PyObject* value )
+{
+	if( PySet_Discard( set, value ) < 0 )
+	{
+		printAndClearError();
+	}
+}
+
+
+inline void safeCallObject( PyObject* cb, PyObject* args, PyObject* kwargs )
+{
+	cppy::ptr ignored( PyObject_Call( cb, args, kwargs ) );
+	if( !ignored )
+	{
+		printAndClearError();
+	}
+}
+
+} // namespace
+
 
 void CallbackSet::add( PyObject* callback )
 {
