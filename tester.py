b26ed1afad40e925c6eaef167fe606b767cc797d
S. Chris Colbert <sccolbert@gmail.com>
2014-09-07 16:18:15 -0400

    quick test  of thread-safety of Atom.sender()

--- tester.py
+++ tester.py
@@ -1,35 +1,83 @@
-from atom.atom import *
+from atom.api import *
+import threading
+import time
 
 
-class Foo(Atom):
-    foo = Member()
-    bar = Member()
-    apple = Member()
-    mellon = Member()
-    green = Member()
-    yellow = Member()
-    blue = Member()
+l = threading.Lock()
 
 
-class Bar(object):
-    pass
+class Foo1(Atom):
+    a = Signal()
 
 
-foo = Foo()
-bar = Bar()
+class Foo2(Atom):
+    a = Signal()
 
-foo.foo = 1
-foo.bar = 2
-foo.apple = 3
-foo.mellon = 4
-foo.green = 5
-foo.yellow = 6
-foo.blue = 7
 
-bar.foo = 1
-bar.bar = 2
-bar.apple = 3
-bar.mellon = 4
-bar.green = 5
-bar.yellow = 6
-bar.blue = 7
+class Bar1(Atom):
+    a = Signal()
+
+
+class Bar2(Atom):
+    a = Signal()
+
+
+f1 = Foo1()
+f2 = Foo2()
+b1 = Bar1()
+b2 = Bar2()
+
+
+def p1():
+    s = ('foo2', Atom.sender())
+    with l:
+        print s
+
+
+
+def p2():
+    for i in range(10):
+        s = ('foo1', Atom.sender())
+        with l:
+            print s
+        time.sleep(0.001)
+        f2.a()
+        s = ('foo1', Atom.sender())
+        with l:
+            print s
+
+
+def p3():
+    s = ('bar2', Atom.sender())
+    with l:
+        print s
+
+
+def p4():
+    for i in range(10):
+        s = ('bar1', Atom.sender())
+        with l:
+            print s
+        time.sleep(0.001)
+        b2.a()
+        s = ('bar1', Atom.sender())
+        with l:
+            print s
+
+
+f1.a.connect(p2)
+f2.a.connect(p1)
+
+
+b1.a.connect(p4)
+b2.a.connect(p3)
+
+
+def worker(ob):
+    ob.a()
+
+
+t = threading.Thread(target=worker, args=(f1,))
+t.daemon = True
+t.start()
+worker(b1)
