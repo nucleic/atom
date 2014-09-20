4c27d4e7e39a0890067392901711e0245434b6ab
S. Chris Colbert <sccolbert@gmail.com>
2014-09-20 14:55:44 -0400

    add AtomList header

---
+++ atom/src/atomlist.h
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+#pragma once
+
+#include <Python.h>
+
+
+namespace atom
+{
+
+// POD struct - all member fields are considered private
+struct AtomList
+{
+	PyListObject list;
+	PyObject* m_value_type;
+
+	static PyTypeObject TypeObject;
+
+	static bool Ready();
+
+	static bool TypeCheck( PyObject* ob )
+	{
+		return PyObject_TypeCheck( ob, &TypeObject ) != 0;
+	}
+
+	static PyObject* Create( PyObject* value_type );
+};
+
+} // namespace atom
