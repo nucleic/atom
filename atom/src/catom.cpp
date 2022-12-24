d27e0b36e9fa03a8b7fadf624f036f48265ba44a
frm dstryr <frmdstryr@protonmail.com>
2022-12-23 20:46:13 -0500

    Move __getstate__ and __setstate_ to catom

--- atom/src/catom.cpp
+++ atom/src/catom.cpp
@@ -21,8 +21,8 @@
 #include "methodwrapper.h"
 #include "packagenaming.h"
 #include "utils.h"
+#include "member.h"
 
-#define member_cast( o ) reinterpret_cast<Member*>( o )
 #define signal_cast( o ) reinterpret_cast<Signal*>( o )
 #define pymethod_cast( o ) reinterpret_cast<PyMethodObject*>( o )
 
@@ -370,6 +370,77 @@ CAtom_sizeof( CAtom* self, PyObject* args )
     return PyLong_FromSsize_t( size );
 }
 
+PyObject*
+CAtom_getstate( CAtom* self )
+{
+    cppy::ptr stateptr = PyDict_New();
+    if ( !stateptr )
+        return PyErr_NoMemory();  // LCOV_EXCL_LINE
+
+    cppy::ptr selfptr(pyobject_cast(self), true);
+
+    // Copy __dict__ if present. Using hasattr / getattr makes it slower
+    // than the py version hence the _PyObject_GetDictPtr.
+    if ( PyObject** dict = _PyObject_GetDictPtr( selfptr.get() ) )
+    {
+        if ( PyDict_Update( stateptr.get(), dict[0] ) )
+            return 0;
+    }
+
+    // Copy __slots__ if present. This assumes copyreg._slotnames was called
+    // during AtomMeta's initialization
+    {
+        cppy::ptr typeptr = PyObject_Type(selfptr.get());
+        if (!typeptr)
+            return 0;
+        cppy::ptr slotnamesptr = typeptr.getattr("__slotnames__");
+        if (!slotnamesptr.get())
+            return 0;
+        if (!PyList_CheckExact(slotnamesptr.get()))
+            return cppy::system_error( "slot names" );
+        for ( Py_ssize_t i=0; i < PyList_GET_SIZE(slotnamesptr.get()); i++ )
+        {
+            PyObject *name = PyList_GET_ITEM(slotnamesptr.get(), i);
+            cppy::ptr value = selfptr.getattr(name);
+            if (!value || PyDict_SetItem(stateptr.get(), name, value.get()) )
+                return  0;
+        }
+    }
+
+    cppy::ptr membersptr = selfptr.getattr("__atom_members__");
+    if ( !membersptr || !PyDict_CheckExact( membersptr.get() ) )
+        return cppy::system_error( "atom members" );
+
+    PyObject *name, *member;
+    Py_ssize_t pos = 0;
+    while ( PyDict_Next(membersptr.get(), &pos, &name, &member) ) {
+        cppy::ptr value = selfptr.getattr(name);
+        if (!value || PyDict_SetItem(stateptr.get(), name, value.get()) )
+            return  0;
+    }
+
+    return stateptr.release();
+}
+
+PyObject*
+CAtom_setstate( CAtom* self, PyObject* args )
+{
+    if( PyTuple_GET_SIZE( args ) != 1 )
+        return cppy::type_error( "__setstate__() takes exactly one argument" );
+    PyObject* state = PyTuple_GET_ITEM( args, 0 );
+    cppy::ptr itemsptr = PyMapping_Items(state);
+    if ( !itemsptr )
+        return 0;
+    cppy::ptr selfptr(pyobject_cast(self), true);
+    for ( Py_ssize_t i = 0; i < PyMapping_Size(state); i++ ) {
+        PyObject* item = PyList_GET_ITEM(itemsptr.get(), i);
+        PyObject* key = PyTuple_GET_ITEM(item , 0);
+        PyObject* value = PyTuple_GET_ITEM(item , 1);
+        if ( !selfptr.setattr(key, value) )
+            return 0;
+    }
+    Py_RETURN_NONE;
+}
 
 static PyMethodDef
 CAtom_methods[] = {
@@ -393,6 +464,10 @@ CAtom_methods[] = {
       "Freeze the atom to prevent further modifications to its attributes." },
     { "__sizeof__", ( PyCFunction )CAtom_sizeof, METH_NOARGS,
       "__sizeof__() -> size of object in memory, in bytes" },
+    { "__getstate__", ( PyCFunction )CAtom_getstate, METH_NOARGS,
+      "The base implementation of the pickle getstate protocol." },
+    { "__setstate__", ( PyCFunction )CAtom_setstate, METH_VARARGS,
+      "The base implementation of the pickle setstate protocol." },
     { 0 } // sentinel
 };
 
---
+++ tests/test_get_set_state.py
@@ -0,0 +1,259 @@
+# --------------------------------------------------------------------------------------
+# Copyright (c) 2022, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# --------------------------------------------------------------------------------------
+import copyreg
+from datetime import datetime
+
+import pytest
+
+from atom.api import Atom, Bool, Float, Int, List, Range, Str, Typed
+
+try:
+    import pytest_benchmark  # noqa: F401
+
+    BENCHMARK_INSTALLED = True
+except ImportError:
+    BENCHMARK_INSTALLED = False
+
+
+class AtomBase(Atom):
+    def __getstate_py__(self):
+        state = {}
+        state.update(getattr(self, "__dict__", {}))
+        slots = copyreg._slotnames(type(self))
+        if slots:
+            for name in slots:
+                state[name] = getattr(self, name)
+        for key in self.members():
+            state[key] = getattr(self, key)
+        return state
+
+    def __setstate_py__(self, state):
+        for key, value in state.items():
+            setattr(self, key, value)
+
+
+def test_getstate_member_slots_error():
+    class Test(Atom):
+        __slots__ = ("a", "b")
+
+    Test.__slotnames__ = None
+    v = Test()
+    with pytest.raises(SystemError):
+        v.__getstate__()
+
+
+def test_getstate_member_error():
+    class Test(Atom):
+        created = Typed(datetime, factory=float)
+
+    v = Test()
+    with pytest.raises(TypeError):
+        v.__getstate__()
+
+
+def test_getstate():
+    class Test(Atom):
+        x = Int(3)
+        y = Int(2)
+
+    t = Test()
+    t.__getstate__() == {"x": 3, "y": 2}
+
+
+@pytest.mark.skipif(not BENCHMARK_INSTALLED, reason="benchmark is not installed")
+@pytest.mark.benchmark(group="getstate")
+@pytest.mark.parametrize("fn", ("c", "py"))
+def test_bench_getstate(benchmark, fn):
+    class Test(AtomBase):
+        first_name = Str("First")
+        last_name = Str("Last")
+        age = Range(low=0)
+        debug = Bool(False)
+        items = List(default=[1, 2, 3])
+
+    expected = {
+        "first_name": "First",
+        "last_name": "Last",
+        "age": 0,
+        "debug": False,
+        "items": [1, 2, 3],
+    }
+    p = Test()
+    if fn == "py":
+        func = p.__getstate_py__
+    else:
+        func = p.__getstate__
+
+    def task():
+        assert func() == expected
+
+    benchmark(task)
+
+
+@pytest.mark.skipif(not BENCHMARK_INSTALLED, reason="benchmark is not installed")
+@pytest.mark.benchmark(group="loopback")
+@pytest.mark.parametrize("fn", ("c", "py"))
+def test_bench_loopback(benchmark, fn):
+    class Test(AtomBase):
+        first_name = Str("First")
+        last_name = Str("Last")
+        age = Range(low=0)
+        debug = Bool(False)
+        items = List(default=[1, 2, 3])
+
+    expected = {
+        "first_name": "First",
+        "last_name": "Last",
+        "age": 0,
+        "debug": False,
+        "items": [1, 2, 3],
+    }
+    if fn == "py":
+
+        def task():
+            t = Test()
+            t.__setstate_py__(expected)
+            assert t.__getstate_py__() == expected
+
+    else:
+
+        def task():
+            t = Test()
+            t.__setstate__(expected)
+            assert t.__getstate__() == expected
+
+    benchmark(task)
+
+
+@pytest.mark.skipif(not BENCHMARK_INSTALLED, reason="benchmark is not installed")
+@pytest.mark.benchmark(group="getstate-dict")
+@pytest.mark.parametrize("fn", ("c", "py"))
+def test_bench_getstate_dict(benchmark, fn):
+    class Foo:
+        def __init__(self):
+            self.count = 1
+
+    class Test(AtomBase, Foo):
+        title = Str("Title")
+        enabled = Bool(True)
+        category = Str("Main")
+        tags = List(default=["foo", "bar"])
+
+    expected = {
+        "count": 1,
+        "title": "Title",
+        "enabled": True,
+        "category": "Main",
+        "tags": ["foo", "bar"],
+    }
+
+    p = Test()
+    p.count = 1
+    if fn == "py":
+        func = p.__getstate_py__
+    else:
+        func = p.__getstate__
+
+    def task():
+        assert func() == expected
+
+    benchmark(task)
+
+
+@pytest.mark.skipif(not BENCHMARK_INSTALLED, reason="benchmark is not installed")
+@pytest.mark.benchmark(group="getstate-slots")
+@pytest.mark.parametrize("fn", ("c", "py"))
+def test_bench_getstate_slots(benchmark, fn):
+    class Test(AtomBase):
+        __slots__ = ("foo", "bar")
+        name = Str("Name")
+        enabled = Bool(True)
+        rating = Float()
+        created = Typed(datetime)
+        tags = List(default=["foo", "bar"])
+
+    now = datetime.now()
+    expected = {
+        "foo": 1,
+        "bar": True,
+        "name": "Name",
+        "enabled": True,
+        "rating": 0.0,
+        "created": now,
+        "tags": ["foo", "bar"],
+    }
+
+    p = Test()
+    p.foo = 1
+    p.bar = True
+    p.created = now
+    if fn == "py":
+        func = p.__getstate_py__
+    else:
+        func = p.__getstate__
+
+    def task():
+        assert func() == expected
+
+    benchmark(task)
+
+
+@pytest.mark.skipif(not BENCHMARK_INSTALLED, reason="benchmark is not installed")
+@pytest.mark.benchmark(group="state")
+@pytest.mark.parametrize("fn", ("c", "py"))
+def test_bench_setstate(benchmark, fn):
+    class Test(AtomBase):
+        x = Int()
+        y = Int()
+
+    if fn == "py":
+
+        def task():
+            t = Test()
+            t.__setstate_py__({"x": 1, "y": 2})
+            assert t.x == 1
+            assert t.y == 2
+
+    else:
+
+        def task():
+            t = Test()
+            t.__setstate__({"x": 1, "y": 2})
+            assert t.x == 1
+            assert t.y == 2
+
+    benchmark(task)
+
+
+def test_setstate():
+    class Test(Atom):
+        x = Int()
+        y = Int()
+
+    t = Test()
+    t.__setstate__({"x": 1, "y": 2})
+    assert t.x == 1
+    assert t.y == 2
+
+
+def test_setstate_errors(caplog):
+    class Test(AtomBase):
+        x = Int()
+        y = Int()
+
+    t = Test()
+    with pytest.raises(TypeError):
+        t.__setstate__()  # Incorrect number of args
+    with pytest.raises(TypeError):
+        t.__setstate__({}, False)  # Incorrect number of args
+    with pytest.raises(AttributeError):
+        t.__setstate__(None)  # Not a mapping (no items() method)
+    with pytest.raises(AttributeError):
+        t.__setstate__(["z"])  # Not a mapping (has no items() method)
+    with pytest.raises(AttributeError):
+        t.__setstate__({"z": 3})  # Invalid attribute
